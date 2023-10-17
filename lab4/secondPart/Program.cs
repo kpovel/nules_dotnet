17d47ae230c629f5cddaea9138eb9265fde972b9
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-17 11:34:28 +0300

    solve the second part of the lab

---
+++ lab4/secondPart/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace KeyEvent
+{
+    class Program
+    {
+        public delegate void KeyPressEvent();
+
+        public static event KeyPressEvent? OnKeyPress;
+
+        static void Main(string[] args)
+        {
+            OnKeyPress += DisplayFullName;
+
+            Console.WriteLine("Press a key...");
+
+            while (true)
+            {
+                var keyInfo = Console.ReadKey();
+
+                if (Char.ToLower(keyInfo.KeyChar) == 'p')
+                {
+                    OnKeyPress?.Invoke();
+                }
+            }
+        }
+
+        static void DisplayFullName()
+        {
+            Console.WriteLine("\nPavlo");
+        }
+    }
+}
+
